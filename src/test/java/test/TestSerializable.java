2b74f709ce9f778a512d0eb72d06635a03c5bb67
yushuanghe <yushuanghe92@outlook.com>
2017-03-31 16:12:01 +0800

    测试String

--- src/test/java/test/TestSerializable.java
+++ src/test/java/test/TestSerializable.java
@@ -11,5 +11,26 @@ public class TestSerializable implements Serializable {
 
     public static void main(String[] args) {
         System.out.println("大力出奇迹！");
+        System.out.println(new String("abc").equals(new String("abc")));
+        System.out.println(new StringBuffer("abc").equals(new StringBuffer("abc")));
+        System.out.println("-----我是分割线-----");
+
+        String a = "abcd";
+        String b = "ab" + "cd";
+        System.out.println(a == b);//true
+        String t = "cd";
+        String c = "ab" + t;
+        System.out.println(a == c);//false
+        final String s = "cd";
+        String d = "ab" + s;
+        System.out.println(a == d);//true
+        String r = "ab";
+        String e = r + t;
+        System.out.println(a == e);//false
+        System.out.println(e.intern() == a);//true
+        String f = new String("abcd");
+        System.out.println(a == f);//false
+        System.out.println(f.intern() == a);//true
+        System.out.println(f.intern() == a.intern());//true
     }
 }
