0023efdc678a5ea87a8fdfc479caf4911f1fa715
yushuanghe <yushuanghe92@outlook.com>
2018-03-18 23:17:13 +0800

    增加 scala 依赖

--- src/main/scala/com/shuanghe/scala/TestMain.scala
+++ src/main/scala/com/shuanghe/scala/TestMain.scala
@@ -4,12 +4,12 @@ import scala.beans.BeanProperty
 
 object TestMain {
     def main(args: Array[String]): Unit = {
-        val a=new TestMain
+        val a = new TestMain
         a.setA("haha")
         println(a.getA)
     }
 }
 
-class TestMain{
-    @BeanProperty var a:String= _
+class TestMain {
+    @BeanProperty var a: String = _
 }
